f8fda91075aac89c9b04c22dbf6892a0a700a438
liuzhipeng <liuzhipengdongjing@gmail.com>
2023-03-12 17:57:50 +0900

    demo完成 多数据未完成

--- src/main/java/com/doumichi/pdfServer/controller/apiController.java
+++ src/main/java/com/doumichi/pdfServer/controller/apiController.java
@@ -3,13 +3,20 @@ package com.doumichi.pdfServer.controller;
 import com.alibaba.fastjson.JSON;
 import com.doumichi.pdfServer.entity.Tuser;
 import com.doumichi.pdfServer.service.InfoService;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class apiController {
@@ -19,8 +26,20 @@ public class apiController {
 
     @ResponseBody
     @RequestMapping("getUserList")
-    private String getUserList(){
+    private String getUserList() throws Exception{
         List<Tuser> tuserList = infoService.getUserList();
+
+        // jasperreport pdf出力
+        String userId = tuserList.get(0).getUser();
+        String password = tuserList.get(0).getPassword();
+
+        Map param = new HashMap();
+        param.put("userId",userId);
+        param.put("password",password);
+        InputStream inputStream = apiController.class.getClassLoader().getResourceAsStream("DuomichiUserReport.jasper");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream,param,new JREmptyDataSource());
+        JasperExportManager
+                .exportReportToPdfFile(jasperPrint,"/Users/liuzhipeng/DuomichiUserReport.pdf");
         return JSON.toJSONString(tuserList);
     }
 }
